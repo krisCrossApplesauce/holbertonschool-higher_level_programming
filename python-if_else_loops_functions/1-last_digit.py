367e0943729582020fd81a574690ac1b31a3964e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-08 14:20:27 -0700

    1-last_digit.py: fixed if number is negative, last digit is considered negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 last = int(repr(number)[-1])
-if last > 5:
-    print(f"Last digit of {number} is {last} and is greater than 5")
-elif last == 0:
+if last == 0:
     print(f"Last digit of {number} is {last} and is 0")
+elif number < 0:
+    print(f"Last digit of {number} is -{last} and is less than 6 and not 0")
+elif last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
 else:
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
