d97f43db5b836b7b0caee4c65a5abcaa95832793
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-10 15:37:23 -0700

    1-last_digit.py: had a typo and I changed how i defined last bc i think that's where the issue is

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int(repr(number)[-1])
 if number < 0:
-    last = last * -1
+    last = number % -10
+else:
+    last = number % 10
 print(f"Last digit of {number} is {last} and is", end=' ')
 if last == 0:
     print(f"0")
 elif last < 6:
     print(f"less than 6 and not 0")
 else:
-    print(f"is greater than 5")
+    print(f"greater than 5")
