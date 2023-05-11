c57c5dfeab033158ed227629feb5cbe3f058914e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-10 19:10:41 -0700

    0: did the __name__ == ''__main__'' thing it seems it wants me to do, we'll see what's next

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
+if __name__ == "__main__":
+    add = __import__('add_0').add
 
-a = 1
-b = 2
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
