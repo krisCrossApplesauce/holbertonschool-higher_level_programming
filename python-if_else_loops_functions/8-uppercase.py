f3d4614d1842f8adbd90eae86fbe8f622de120a4
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-10 17:27:44 -0700

    8-uppercase.py (w/ 8-main.py): I was gonna change it to be based off of the way Caramon did it, but Cason's was way more similar to what I already had, I just didn't think to redefine char, smart

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import string
 def uppercase(str):
-    x = 0
-    for i in str:
-        val = ord(str[x])
+    for char in str:
+        val = ord(char)
         if val >= 97 and val <= 122:
-            print("{}".format(string.ascii_uppercase[val - 97]), end="")
-        else:
-            print("{}".format(str[x]), end="")
-        x = x + 1
+            char = chr(val - 32)
+        print("{}".format(char), end='')
+    else:
+        print()
