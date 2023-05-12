4d2d58f49577e4807bb558ac60f3dfdbdf020f93
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-12 12:49:49 -0700

    6: if len(matrix) == 0 or len(i) == 0, print("") (new line)

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print("")
     for i in matrix:
+        if len(i) == 0:
+            print("")
         for ii in range(len(i)):
             print("{:d}".format(i[ii]), end=" " if ii + 1 < len(i) else "\n")
