5250de91186579e1d434cb8ab932869c60c97920
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-12 12:43:52 -0700

    6-print_matrix_integer.py (w/ 6-main.py)

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for ii in range(len(i)):
+            print("{}".format(i[ii]), end=" " if ii + 1 < len(i) else "\n")
