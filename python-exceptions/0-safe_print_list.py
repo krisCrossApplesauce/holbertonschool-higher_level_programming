58e621b63ee8340e31f85a5a1c001cc25a49c94f
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-16 15:29:42 -0700

    0&1: changed while loop to for (bc more applicable) in 0 and rid of loop all together in 1 bc don't need it

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     a = 0
-    i = 0
-    while i in range(x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
             a += 1
         except IndexError:
             break
-        i += 1
     print("")
     return a
--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    while True:
-        try:
-            print("{:d}".format(value))
-            return True
-        except ValueError:
-            return False
-        except TypeError:
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
+    except TypeError:
+        return False
