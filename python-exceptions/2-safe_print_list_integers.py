e8c34373519dd98dcb9462d62aff3f03262351be
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-16 15:36:24 -0700

    2: i cannot believe it really wants me to leave the IndexError without an except >:(

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             a += 1
-        except:
+        except ValueError:
+            continue
+        except TypeError:
             continue
     print("")
     return a
