32dc7463061344411dba19da42f3fdbba57a9300
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-16 14:08:49 -0700

    1: created two separate except things for each error

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,7 @@ def safe_print_integer(value):
         try:
             print("{:d}".format(value))
             return True
-        except ValueError or TypeError:
+        except ValueError:
+            return False
+        except TypeError:
             return False
