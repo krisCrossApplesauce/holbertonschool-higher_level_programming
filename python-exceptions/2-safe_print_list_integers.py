67050c2c1226fae3bcf4f2d5b2445c86f2861dde
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-16 15:13:42 -0700

    2: vs code wasn't saving what i had from the text editor -_- very rude, i have some crude words to say about it

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
     a = 0
-    while i in range(x):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             a += 1
-        except IndexError:
-            break
-        except ValueError:
-            pass
-        except TypeError:
-            pass
-        i += 1
+        except:
+            continue
+    print("")
+    return a
+
