50ece83f0051ede387cb43ec63bcfc381d282538
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-16 13:18:37 -0700

    12: fixed the line length -_-

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,11 @@ def roman_to_int(roman_string):
     n = 0
     if isinstance(roman_string, str) == False:
         return n
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
-        if i + 1 != len(roman_string) and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
-            n += roman_dict[roman_string[i + 1]] - roman_dict[roman_string[i]]
-        elif i == 0 or roman_dict[roman_string[i]] <= roman_dict[roman_string[i - 1]]:
-            n += roman_dict[roman_string[i]]
+        if i + 1 != len(roman_string) and \
+                d[roman_string[i]] < d[roman_string[i + 1]]:
+            n += d[roman_string[i + 1]] - d[roman_string[i]]
+        elif i == 0 or d[roman_string[i]] <= d[roman_string[i - 1]]:
+            n += d[roman_string[i]]
     return n
