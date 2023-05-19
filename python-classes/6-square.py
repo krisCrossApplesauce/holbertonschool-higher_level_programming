d41760677cd70f9dd9908cde36ed70a88115ce4e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-19 12:56:57 -0700

    6: somehow also forgot to check that both elements in the tuple are both ints and >= 0. also rid of a whitespace

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -11,7 +11,10 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        if type(position) is not tuple or len(position) != 2:
+        if type(position) is not tuple or len(position) != 2 \
+        or type(position[0]) is not int or type(position[1]) is not int \
+            or position[0] < 0 or position[1] < 0 \
+                or type(position[0]) is not int or type(position[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
@@ -36,7 +39,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @property
     def position(self):
         return self.__position
@@ -52,7 +55,9 @@ class Square:
 
     @size.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value) is not tuple or len(value) != 2 \
+            or type(value[0]) is not int or type(value[1]) is not int \
+                or value[0] < 0 or value[1] < 0:
+                raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
