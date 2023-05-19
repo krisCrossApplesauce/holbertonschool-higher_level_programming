ab5aa069e9edd7d01287161e95385ff6e3ec1de6
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-19 11:53:47 -0700

    4-square.py (w/ 4-main.py)

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square (based on 3-square.py)"""
+
+
+class Square:
+    """square but with a getter and setter"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
