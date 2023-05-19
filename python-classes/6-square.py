05cacffa83ad22c385167931e7f2b416c2437a80
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-19 13:29:27 -0700

    6: PLEASE PEP8, WHY WON'T YOU BE APPEASED, JUST LET ME BEEEEEE

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,7 +14,7 @@ class Square:
         if type(position) is not tuple or len(position) != 2 \
             or type(position[0]) is not int or type(position[1]) is not int \
                 or position[0] < 0 or position[1] < 0:
-                raise TypeError("position must be a tuple" \
+                raise TypeError("position must be a tuple"
                         + " of 2 positive integers")
         else:
             self.__position = position
@@ -57,7 +57,7 @@ class Square:
         if type(value) is not tuple or len(value) != 2 \
             or type(value[0]) is not int or type(value[1]) is not int \
                 or value[0] < 0 or value[1] < 0:
-                raise TypeError("position must be a tuple" \
+                raise TypeError("position must be a tuple"
                         + " of 2 positive integers")
         else:
             self.__position = value
