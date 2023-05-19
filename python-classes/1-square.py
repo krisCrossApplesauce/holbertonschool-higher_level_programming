2477f6528045b75eaf8e3fbafe0f3474d0d08fd4
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-19 10:25:17 -0700

    1-square.py (w/ 1-main.py): i don't understand anything I'm doing, but supposedly this works

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square:"""
+
+class Square:
+    """wow, it's a square"""
+    __size = None
+
+    def __init__(self, new_size=None):
+        self.__size = new_size
