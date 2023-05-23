09f7f029ad48a72b4efa165e3884d62f6b676335
krisCrossApplesauce <karisrr2203@gmail.com>
2023-05-23 12:41:34 -0700

    1: ??

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -20,16 +20,14 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
