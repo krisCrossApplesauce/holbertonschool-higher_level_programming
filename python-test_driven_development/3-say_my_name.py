7346619a35d3c926f3202137fbc7b73af854471d
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-01 10:58:51 -0700

    3: added documentation in function and added a test where run func with no args

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """it prints a name and that's about it"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
