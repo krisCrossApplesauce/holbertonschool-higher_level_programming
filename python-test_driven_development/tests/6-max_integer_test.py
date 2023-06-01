6e1327d78a65348e2a30587db9049e24cad5c96a
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-01 12:07:30 -0700

    6-max_integer.py (w/ 6-main.py) and test/6-max_integer.py: unittest is empty so i can see what it wants me to test for bc im a bit confused and it's easier to move forward with a direction to move in

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""unittests for 6-max_integer"""
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """class for testing 6-max_integer"""
+    pass
