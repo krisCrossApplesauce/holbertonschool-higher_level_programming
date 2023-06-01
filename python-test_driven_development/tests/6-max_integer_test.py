3badd8cc11393195f67aa002b92a016c43175aae
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-01 12:27:58 -0700

    6: have i succeeded?..

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,4 +6,23 @@ max_integer = __import__("6-max_integer").max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """class for testing 6-max_integer"""
-    pass
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+
+    def test_max_in_middle(self):
+        self.assertEqual(max_integer([4, 7, 3]), 7)
+
+    def test_one_negative(self):
+        self.assertEqual(max_integer([2, 3, -5, 4]), 4)
+
+    def test_all_negative(self):
+        self.assertEqual(max_integer([-5, -4, -2, -3, -1, -6]), -1)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_empty(self):
+        self.assertEqual(max_integer(), None)
