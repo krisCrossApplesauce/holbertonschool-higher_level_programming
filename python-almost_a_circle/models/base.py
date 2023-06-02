d4f2618138d4b0c190198e00bfcfe667837f55a5
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-02 10:47:33 -0700

    models folder, models/__init__.py, and models/base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a class called Base """
+
+
+class Base:
+    """ Base class of all other classes in this project """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
