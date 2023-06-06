d9fe2daf06bf5bc5aa5f4403f019c544fdfc6f08
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-06 11:48:12 -0700

    models/square.py (w/ 11-main.py)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,6 +22,24 @@ class Square(Rectangle):
             self.width = value
             self.height = value
 
+    def update(self, *args, **kwargs):
+        """ assigns an assignment to each attribute """
+        attributes = ["id", "size", "x", "y"]
+        if args is not None and len(args) > 0:
+            for attr, value in zip(attributes, args):
+                if attr == "size":
+                    self.width = value
+                    self.height = value
+                else:
+                    setattr(self, attr, value)
+        elif kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "size":
+                    self.width = value
+                    self.height = value
+                else:
+                    setattr(self, key, value)
+
     def __str__(self):
         """ it returns info about the square """
         return "[Square] ({}) {}/{} - {}".format(
