d449c3d333bccbd222adcb88339f9ede07075069
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-06-29 14:24:36 -0500

    1: ALL -> ALL PRIVILEGES & +IF NOT EXISTS

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- write a script that creates the MySQL server user user_0d_1
-GRANT ALL TO user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON *.* TO IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
