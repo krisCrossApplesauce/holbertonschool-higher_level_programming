9023cf637c04651c3b6124eeba6f48164649b583
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-06-29 15:37:11 -0500

    7: forgot a , D',

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
