25033c46e1b2b36c4e64e0a42a1361911cf88591
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-06-30 12:18:48 -0500

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 -- import database dump from hbtn_0d_tvshows and list all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id ORDER BY tv_shows.title, tv_shows_genre.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genre.genre_id ASC;
