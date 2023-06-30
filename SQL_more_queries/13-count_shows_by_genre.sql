e2a0c7c4b6ce83b63129559ae90c8fe49cae277e
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-06-30 13:41:20 -0500

    13: didn't realize tv_genres exists, but this is definitely what i was thinking of (except GROUP BY)

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- import same database from 10-12, and list all genres from hbtn_0d_tvshows and display the nunmber of shows linked to each
-SELECT tv_show_genres.genre_id AS genre, count(tv_shows) AS number_of_shows
+SELECT DISTINCT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.show_id=tv_show.id
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_of_shows DESC;
