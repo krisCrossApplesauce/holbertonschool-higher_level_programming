de663e9e610f3ddd228c00aabc1acc6941d47410
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-03 18:07:10 -0700

    0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" write a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+def list_states():
+    """ does something """
+    db = MySQLdb.connect(host="localhost", username=sys.argv[0], password=sys.argv[1], name=sys.argv[2])
+
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    db.close()
+
+
+if __name__ = "__main__":
+    list_states()
