a0edd52a64cc79a90642b5675b7c8e47c278a26e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-03 18:49:07 -0700

    1: pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,11 +10,13 @@ import sys
 
 def filter_states():
     """ does something """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'
+                ORDER BY states.id ASC")
     rows = cur.fetchall()
     for r in rows:
         print(r)
