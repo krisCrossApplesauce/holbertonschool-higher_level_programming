1146ef226dd34e0fa1f7092bdb46d1c591875846
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-03 18:19:48 -0700

    0: started argv at [1] instead of [0] so doesn't include ./0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 
 def list_states():
     """ does something """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[0], password=sys.argv[1], database=sys.argv[2])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
 
     curs = db.cursor()
     curs.execute("SELECT * FROM states ORDER BY states.id ASC")
