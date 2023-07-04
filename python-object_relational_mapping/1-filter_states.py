3519fd5dc96b3a16a797addeec4abec18f5e7a4b
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-03 18:53:55 -0700

    1: screw pycodestyle, im trying to hurry up and understand it, not make it look a certain way -_-

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,8 +15,7 @@ def filter_states():
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'
-                ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for r in rows:
         print(r)
