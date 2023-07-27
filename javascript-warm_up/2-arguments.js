d3e99d1f896363b5ab71f180ef53bb7014f334b2
root <karisrr2203@gmail.com>
2023-07-27 11:36:32 -0700

    2-arguments.js: must start counting at 1 opposed to 0 or something

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log();
+const process = require('process');
+const args = process.argv;
+if (args.length == 2) {
+	console.log("No argument");
+} else if (args.length == 3) {
+	console.log("Argument found");
+} else {
+	console.log("Arguments found");
+}
