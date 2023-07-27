180d174c0eaf0e388b7d8d618fee1017d40d629d
root <karisrr2203@gmail.com>
2023-07-27 12:13:52 -0700

    4: when multiple things in console.log, automatically puts spaces between them

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,9 +2,9 @@
 const process = require('process');
 const args = process.argv;
 if (args[3]) {
-  console.log(args[2], ' is ', args[3]);
+  console.log(args[2], 'is', args[3]);
 } else if (args[2]) {
-  console.log(args[2], ' is undefined');
+  console.log(args[2], 'is undefined');
 } else {
   console.log('undefined is undefined');
 }
