c88cc62735361c56253b651c54293aa70d04e3dd
root <karisrr2203@gmail.com>
2023-07-28 12:40:01 -0700

    11: needed to parseInt the arguments

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,17 @@ const args = process.argv;
 if (args.length < 2) {
   console.log(0);
 }
-function sec_big (a) {
+function secBig (a) {
   let n = 0;
-  let big = a[2];
+  let big = parseInt(a[2]);
   for (let i = 3; i < a.length; i++) {
-    if (a[i] > big) {
+    if (parseInt(a[i]) > big) {
       n = big;
-      big = a[i];
-    } else if (a[i] > n) {
-      n = a[i];
+      big = parseInt(a[i]);
+    } else if (parseInt(a[i]) > n) {
+      n = parseInt(a[i]);
     }
   }
   console.log(n);
 }
-sec_big(args);
+secBig(args);
