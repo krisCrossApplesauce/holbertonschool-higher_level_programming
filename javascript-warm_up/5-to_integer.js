f469f33cbfe53f5235c174395353ee13fb57cadb
root <karisrr2203@gmail.com>
2023-07-28 10:50:43 -0700

    5: strings must use singlequotes for whatever reason -_-

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const args = process.argv;
 if (parseInt(args[2])) {
-  console.log("My number:", parseInt(args[2]));
+  console.log('My number:', parseInt(args[2]));
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
