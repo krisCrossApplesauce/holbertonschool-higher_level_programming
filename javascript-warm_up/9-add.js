f12c3713684e1b323de45b4029d9f476fc4b3b8f
root <karisrr2203@gmail.com>
2023-07-28 11:59:02 -0700

    9: semistandard

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-function add(a, b) {
-  let c = a + b;
+function add (a, b) {
+  const c = a + b;
   console.log(c);
 }
 add(parseInt(args[2]), parseInt(args[3]));
