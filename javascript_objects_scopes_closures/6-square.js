29362720519f2645fb15abb074487aec4c42a4fc
root <karisrr2203@gmail.com>
2023-07-31 12:46:51 -0700

    6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,20 +3,19 @@ const OriginalSquare = require('./5-square');
 
 class Square extends OriginalSquare {
   constructor (size) {
-    super(size);
+    super(size, size);
   }
 
   charPrint (c) {
-    let width = '';
-    while (width.length < this.size) {
-      if (c) {
-        width += c;
-      } else {
-        width += 'X';
-      }
+    if (c === undefined) {
+      c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
-      console.log(width);
+    let w = '';
+    while (w.length < this.width) {
+      w += c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(w);
     }
   }
 }
