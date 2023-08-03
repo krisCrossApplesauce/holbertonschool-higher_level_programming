e369c0ed2bb6fc14fe1b80b134e7cdf614a06ccf
root <karisrr2203@gmail.com>
2023-08-03 19:45:33 +0000

    3-starwars_title.js, but it claims body["title"] to be undefined

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (err) { console.log(err); }
+  console.log(body["title"]);
+});
