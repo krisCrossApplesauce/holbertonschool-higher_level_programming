1b17861e74ef0dadc1d1ef76afd634bab09c71cd
root <karisrr2203@gmail.com>
2023-08-03 20:32:22 +0000

    4: -_- adjusted the code so that the url for Wedge Antilles is based off of the given url

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 const process = require('process');
+const url = process.argv[2];
 const request = require('request');
-request(process.argv[2], function (err, response, body) {
+const wedgeAntilles = url.substring(0, (url.length - 5)) + 'people/18/';
+request(url, function (err, response, body) {
   if (err) { console.log(err); }
   const filmList = JSON.parse(body).results;
   let count = 0;
   for (const film of filmList) {
     for (const char of film.characters) {
-      if (char === 'https://swapi-api.hbtn.io/api/people/18/') {
+      if (char === wedgeAntilles) {
         count += 1;
       }
     }
