d43c2cd4f076251ccff2e8876fa71c732a5cf59a
root <karisrr2203@gmail.com>
2023-08-03 20:18:25 +0000

    4: semistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,11 @@ const process = require('process');
 const request = require('request');
 request(process.argv[2], function (err, response, body) {
   if (err) { console.log(err); }
-  const film_list = JSON.parse(body).results;
+  const filmList = JSON.parse(body).results;
   let count = 0;
-  for (film of film_list) {
-    for (char of film.characters) {
-      if ("https://swapi-api.hbtn.io/api/people/18/" === char) {
+  for (const film of filmList) {
+    for (const char of film.characters) {
+      if (char === 'https://swapi-api.hbtn.io/api/people/18/') {
         count += 1;
       }
     }
