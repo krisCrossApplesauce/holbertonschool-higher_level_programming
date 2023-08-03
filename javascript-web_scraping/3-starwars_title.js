3bb32ca25b7f09761d48eab1ddc7c3a3ac67b534
root <karisrr2203@gmail.com>
2023-08-03 19:55:57 +0000

    3: semistandard (camel case)

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,6 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (err, response, body) {
   if (err) { console.log(err); }
-  const film_obj = JSON.parse(body);
-  console.log(film_obj.title);
+  const filmObj = JSON.parse(body);
+  console.log(filmObj.title);
 });
