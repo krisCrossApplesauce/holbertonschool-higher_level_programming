b738d3960feeff919a034b93874026d5c267c799
root <karisrr2203@gmail.com>
2023-08-03 21:15:54 +0000

    6-completed_tasks.js: not working but whatever ,'P

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+request(process.argv[2], function (err, response, body) {
+  if (err) { console.log(err); }
+  const taskList = JSON.parse(body);
+  let usrsDoneTasks = {};
+  for (const task of taskList) {
+    if (usrsDoneTasks[task.userId] && (usrsDoneTasks[task.completed] === true)) {
+      count += 1;
+      usrsDoneTasks[task.userId] = count;
+    } else if (usrsDoneTasks[task.completed] === true) {
+      count = 1;
+      usrsDoneTasks[task.userId] = count;
+    } else {
+      count = 0;
+    }
+  }
+  console.log(usrsDoneTasks);
+});
