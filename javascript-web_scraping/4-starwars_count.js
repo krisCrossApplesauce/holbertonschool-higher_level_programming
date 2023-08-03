13ba040590e609ac313e8e4aa32b8a71424f3b45
root <karisrr2203@gmail.com>
2023-08-03 20:39:37 +0000

    4: .include() is a beautiful function, so much easier than what I was doing, wow

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const process = require('process');
-const url = process.argv[2];
 const request = require('request');
-const wedgeAntilles = url.substring(0, (url.length - 5)) + 'people/18/';
-request(url, function (err, response, body) {
+request(process.argv[2], function (err, response, body) {
   if (err) { console.log(err); }
   const filmList = JSON.parse(body).results;
   let count = 0;
   for (const film of filmList) {
     for (const char of film.characters) {
-      if (char === wedgeAntilles) {
+      if (char.includes('/18/')) {
         count += 1;
       }
     }
